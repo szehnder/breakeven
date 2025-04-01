f522b7895b8af20fc4f5faa720109b40c9cf9bfd
szehnder <seanzehnder@gmail.com>
2025-03-31 18:30:46 -0600

    tweaking the action

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,10 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  base: './', // Ensures assets are loaded correctly on GitHub Pages
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+  },
 })
